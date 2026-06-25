7a6db5e4136ff37e139f8b7ad33a05a78f87fab8
Prabal dubey <prabaldubey02@gmail.com>
2026-06-25 23:18:26 +0530

    Time: 15 ms (14.38%), Space: 62.4 MB (21.15%) - LeetHub

---
+++ 0148-sort-list/0148-sort-list.java
@@ -0,0 +1,24 @@
+class Solution {
+    public ListNode sortList(ListNode head) {
+        if (head == null) return null;
+
+        ArrayList<Integer> list = new ArrayList<>();
+
+        ListNode curr = head;
+        while (curr != null) {
+            list.add(curr.val);
+            curr = curr.next;
+        }
+
+        Collections.sort(list);
+
+        curr = head;
+        int i = 0;
+        while (curr != null) {
+            curr.val = list.get(i++);
+            curr = curr.next;
+        }
+
+        return head;
+    }
+}
