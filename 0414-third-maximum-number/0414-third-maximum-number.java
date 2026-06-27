62c0faa36963764a4ff66f6408156532d6044abb
Prabal dubey <prabaldubey02@gmail.com>
2026-06-27 23:37:29 +0530

    Time: 1 ms (98.94%), Space: 44.5 MB (94.87%) - LeetHub

---
+++ 0414-third-maximum-number/0414-third-maximum-number.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int thirdMax(int[] nums) {
+
+        long max = Long.MIN_VALUE;
+        long smax = Long.MIN_VALUE;
+        long tmax = Long.MIN_VALUE;
+
+        for (int num : nums) {
+
+            // Ignore duplicates
+            if (num == max || num == smax || num == tmax)
+                continue;
+
+            if (num > max) {
+                tmax = smax;
+                smax = max;
+                max = num;
+            }
+            else if (num > smax) {
+                tmax = smax;
+                smax = num;
+            }
+            else if (num > tmax) {
+                tmax = num;
+            }
+        }
+
+        return tmax == Long.MIN_VALUE ? (int) max : (int) tmax;
+    }
+}
