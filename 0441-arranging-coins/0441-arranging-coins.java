56ae2825edc998e752634184484e3b3bbb53de2e
Prabal dubey <prabaldubey02@gmail.com>
2026-07-01 14:27:38 +0530

    Time: 1 ms (100%), Space: 42.8 MB (43.57%) - LeetHub

---
+++ 0441-arranging-coins/0441-arranging-coins.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int sqrt(long n){
+        if(n==0) return 0;
+        long lo = 1;long hi = n;
+        while(lo<=hi){
+            long mid = lo + (hi-lo)/2;
+            if(mid==n/mid) return (int)mid;
+            if(mid>n/mid) hi = mid-1;
+            else lo = mid+1;
+        }
+        return (int)hi;
+
+        }
+    
+    public int arrangeCoins(int n) {
+        long m = (long)n;
+        return (sqrt(8*m+1)-1)/2;
+        
+    }
+}
