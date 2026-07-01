157d5cadd9eb8340c66f03793b886b6f80350f03
Prabal dubey <prabaldubey02@gmail.com>
2026-07-01 16:33:14 +0530

    Time: 235 ms (16.19%), Space: 117 MB (78.73%) - LeetHub

---
+++ 2812-find-the-safest-path-in-a-grid/2812-find-the-safest-path-in-a-grid.java
@@ -0,0 +1,104 @@
+import java.util.*;
+
+class Solution {
+
+    int[][] dir = {{1,0},{-1,0},{0,1},{0,-1}};
+
+    public int maximumSafenessFactor(List<List<Integer>> grid) {
+
+        int n = grid.size();
+        int[][] dist = new int[n][n];
+
+        // Initialize distances
+        for (int i = 0; i < n; i++) {
+            Arrays.fill(dist[i], -1);
+        }
+
+        Queue<int[]> q = new LinkedList<>();
+
+        // Add all thieves to queue
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid.get(i).get(j) == 1) {
+                    q.offer(new int[]{i, j});
+                    dist[i][j] = 0;
+                }
+            }
+        }
+
+        // Multi-Source BFS
+        while (!q.isEmpty()) {
+            int[] cur = q.poll();
+
+            int r = cur[0];
+            int c = cur[1];
+
+            for (int[] d : dir) {
+                int nr = r + d[0];
+                int nc = c + d[1];
+
+                if (nr >= 0 && nr < n && nc >= 0 && nc < n && dist[nr][nc] == -1) {
+                    dist[nr][nc] = dist[r][c] + 1;
+                    q.offer(new int[]{nr, nc});
+                }
+            }
+        }
+
+        int low = 0;
+        int high = 2 * n;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (canReach(dist, mid)) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+
+        return high;
+    }
+
+    private boolean canReach(int[][] dist, int safe) {
+
+        int n = dist.length;
+
+        if (dist[0][0] < safe)
+            return false;
+
+        Queue<int[]> q = new LinkedList<>();
+        boolean[][] vis = new boolean[n][n];
+
+        q.offer(new int[]{0, 0});
+        vis[0][0] = true;
+
+        while (!q.isEmpty()) {
+
+            int[] cur = q.poll();
+
+            int r = cur[0];
+            int c = cur[1];
+
+            if (r == n - 1 && c == n - 1)
+                return true;
+
+            for (int[] d : dir) {
+
+                int nr = r + d[0];
+                int nc = c + d[1];
+
+                if (nr >= 0 && nr < n &&
+                    nc >= 0 && nc < n &&
+                    !vis[nr][nc] &&
+                    dist[nr][nc] >= safe) {
+
+                    vis[nr][nc] = true;
+                    q.offer(new int[]{nr, nc});
+                }
+            }
+        }
+
+        return false;
+    }
+}
