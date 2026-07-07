a5cd5ca8047612c51f6f84e1c4f79625e52d5968
Prabal dubey <prabaldubey02@gmail.com>
2026-07-07 17:26:36 +0530

    Time: 10 ms (26.04%), Space: 42.3 MB (15.84%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,8 +1,8 @@
 class Solution {
     public int fib(int n) {
         int ans = 0;
-        if(n==0 ) return 0;
-        if(n==1 || n==2) return 1;
+       
+        if(n==1 || n==0) return n;
         else 
         ans =fib(n-1)+ fib(n-2);
         
