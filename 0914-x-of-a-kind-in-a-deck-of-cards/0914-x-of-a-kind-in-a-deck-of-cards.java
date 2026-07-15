f39ac0fd7e1a66aba08b255391fc60471b45098c
Prabal dubey <prabaldubey02@gmail.com>
2026-07-15 22:21:29 +0530

    Time: 10 ms (81.47%), Space: 47.3 MB (37.18%) - LeetHub

---
+++ 0914-x-of-a-kind-in-a-deck-of-cards/0914-x-of-a-kind-in-a-deck-of-cards.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean hasGroupsSizeX(int[] deck) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+       
+        for (int card : deck) {
+            map.put(card, map.getOrDefault(card, 0) + 1);
+        }
+
+        int gcd = 0;
+
+        for (int freq : map.values()) {
+            gcd = findGCD(gcd, freq);
+        }
+
+        return gcd >= 2;
+    }
+
+    private int findGCD(int a, int b) {
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+}
