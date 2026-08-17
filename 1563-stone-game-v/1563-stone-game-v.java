e38a2f8f1368f24300b59bcc1df747c4fe54d5d3
Prabal dubey <prabaldubey02@gmail.com>
2026-08-17 19:24:49 +0530

    Time: 222 ms (91.11%), Space: 47.9 MB (37.26%) - LeetHub

---
+++ 1563-stone-game-v/1563-stone-game-v.java
@@ -0,0 +1,60 @@
+class Solution {
+    int[][] dp;
+    int[] prefix;
+    int[] nums;
+
+    public int stoneGameV(int[] stoneValue) {
+        nums = stoneValue;
+        int n = nums.length;
+
+        prefix = new int[n + 1];
+
+        for (int i = 0; i < n; i++) {
+            prefix[i + 1] = prefix[i] + nums[i];
+        }
+
+        dp = new int[n][n];
+
+        for (int[] row : dp) {
+            java.util.Arrays.fill(row, -1);
+        }
+
+        return solve(0, n - 1);
+    }
+
+    int solve(int l, int r) {
+        if (l >= r) {
+            return 0;
+        }
+
+        if (dp[l][r] != -1) {
+            return dp[l][r];
+        }
+
+        int ans = 0;
+
+        for (int k = l; k < r; k++) {
+
+            int left = prefix[k + 1] - prefix[l];
+            int right = prefix[r + 1] - prefix[k + 1];
+
+            if (left < right) {
+                ans = Math.max(ans,
+                        left + solve(l, k));
+            }
+            else if (left > right) {
+                ans = Math.max(ans,
+                        right + solve(k + 1, r));
+            }
+            else {
+                ans = Math.max(ans,
+                        Math.max(
+                            left + solve(l, k),
+                            right + solve(k + 1, r)
+                        ));
+            }
+        }
+
+        return dp[l][r] = ans;
+    }
+}
